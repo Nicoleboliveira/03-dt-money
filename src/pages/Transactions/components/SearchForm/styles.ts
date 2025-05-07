b241fdf87f900c8c93afd7687b0bd6f3be1cdf22
nicole <nicoleboliveira14@gmail.com>
2025-05-07 10:54:43 -0300

    feat(form): aplicar react-hook-form nos formulários

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -25,10 +25,16 @@ export const SearchFormContainer = styled.form`
     color: ${(props) => props.theme["green-300"]};
     font-weight: bold;
     border-radius: 6px;
-    gap: 0.75rem;
+    gap: 0.5rem;
     align-items: center;
+    cursor: pointer;
 
-    &:hover {
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${(props) => props.theme["green-500"]};
       border-color: ${(props) => props.theme["green-500"]};
       color: ${(props) => props.theme["white"]};
